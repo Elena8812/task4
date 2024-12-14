1dae29279b3a59b535148a945d336b475530efa4
Elena8812 <Len4ik8812@gmail.com>
2024-12-14 16:33:27 +1200

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,5 @@
+course_name='Python'
+total_tasks=12
+hours_spent=1.5
+one_task=1.5/12
+print(course_name, "всего задач", total_tasks, "затрачено часов", hours_spent, "среднее время выполнения", one_task )
